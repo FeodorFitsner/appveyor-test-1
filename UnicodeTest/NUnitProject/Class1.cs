e3504821c3bf51735f97d5ccf793d70099ff5481
bzaar <slepov@gmail.com>
2017-03-11 09:58:35 +0000

    Add NUnit test.

---
+++ UnicodeTest/NUnitProject/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NUnitProject
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail("Русские буквы из NUnit.");
+        }
+    }
+}
